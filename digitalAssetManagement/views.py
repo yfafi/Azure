60506fe8ca97ff0d9f46a8edb3221823b685c161
Younes <younes.fafi@hotmail.com>
2022-07-03 20:13:07 +0200

    evol - Form

--- digitalAssetManagement/views.py
+++ digitalAssetManagement/views.py
@@ -6,11 +6,12 @@ def index(request):
 def describeImages(request):
     # 1) Récupération des données depuis le form
     if request.method == 'POST':
-        postData = request.POST['email']
+        titreImg = request.POST['titreImg']
+        fileImg = request.POST['fileImg']
 
     # 2) Requete API pour récupérer les données de L'IA Azure
 
-    data = ['38%','Un mec en skate', 'https://www.grapheine.com/wp-content/uploads/2015/09/nouveau-logo-google-2015.jpg']
+    data = [titreImg,fileImg]
 
     # 3) Récupération des données et render sur la template HTML
     return render(request, 'describe.html', dict(data=data))
